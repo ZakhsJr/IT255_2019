a88585d04b2ea290b519dbe856c6fbe69ee4b5ef
elPyoho <hirgon92@gmail.com>
2018-02-22 22:21:27 +0100

    Domaci Zadatak 01

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-    echo "Ovo je echo string";
+    echo '<a href ="https://www.youtube.com/watch?v=rSycSBYHitc" title="Chromatics - Into the Black" style="text-decoration:none">Psst. This way to song </a>';  
 ?>

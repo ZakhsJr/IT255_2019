07f64907e7d749a6d2fb1f961c8801ab800a93f7
elPyoho <hirgon92@gmail.com>
2019-07-27 13:13:48 +0200

    Homework No. 1

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<h1>
 <?php
-    echo '<a href ="https://www.youtube.com/watch?v=rSycSBYHitc" title="Chromatics - Into the Black" style="text-decoration:none">Psst. This way to song </a>';  
+    echo '<a href ="https://www.youtube.com/watch?v=2e8x3fFZP3Q" title="You & I" style="text-decoration:none; color:#0000ee !important">Psst. This way to song </a>';  
 ?>
+</h1>
